daa82644ea107de51acab760bdf9db3cfec993c4
Will Usher <will@sci.utah.edu>
2019-12-28 12:38:53 -0700

    allow RGBA isosurface colors

--- main.cpp
+++ main.cpp
@@ -197,7 +197,11 @@ void render_images(const std::vector<std::string> &args)
 
     cpp::Material material("scivis", "default");
     if (config.find("isosurface_color") != config.end()) {
-        material.setParam("Kd", get_vec<float, 3>(config["isosurface_color"]));
+        auto color = config["isosurface_color"].get<std::vector<float>>();
+        material.setParam("Kd", vec3f(color[0], color[1], color[2]));
+        if (color.size() == 4) {
+            material.setParam("d", color[3]);
+        }
     } else {
         material.setParam("Kd", vec3f(1.f));
     }
