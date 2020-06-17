49b7709b21c610ce32c8f2d47ef7c9e2d736891d
Will Usher <will.usher@intel.com>
2020-06-17 12:33:36 -0600

    fix num threads var

--- set_ospray_vars.sh
+++ set_ospray_vars.sh
@@ -15,22 +15,22 @@ set_ospray_env_vars() {
     export OSPRAY_SET_AFFINITY=0
 
     CPU_MODEL=`cat /proc/cpuinfo | grep "model name" | uniq`
-    if [ -z "$OSPRAY_THREADS" ]; then
+    if [ -z "$OSPRAY_NUM_THREADS" ]; then
         # If we're on Xeon use all logical cores, if on Phi use only phyiscal cores
         if [ -n "`echo $CPU_MODEL | grep 'Xeon(R)'`" ]; then
             echo "CPU $CPU_MODEL is Xeon, using all logical cores"
-            export OSPRAY_THREADS=$(get_logical_cores)
+            export OSPRAY_NUM_THREADS=$(get_logical_cores)
             export I_MPI_PIN_PROCESSOR_LIST=all
         else
             echo "CPU $CPU_MODEL is Xeon Phi, using all physical cores"
-            export OSPRAY_THREADS=$(get_physical_cores)
+            export OSPRAY_NUM_THREADS=$(get_physical_cores)
             export I_MPI_PIN_PROCESSOR_LIST=allcores
         fi
     else
-        echo "OSPRAY_THREADS set by user to $OSPRAY_THREADS"
+        echo "OSPRAY_NUM_THREADS set by user to $OSPRAY_NUM_THREADS"
     fi
 
-    export OMP_NUM_THREADS=$OSPRAY_THREADS
+    export OMP_NUM_THREADS=$OSPRAY_NUM_THREADS
     source $EMBREE_DIR/embree-vars.sh
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OSPRAY_DIR}/build/install/lib64/
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OPENVKL_DIR}/build/install/lib64/
